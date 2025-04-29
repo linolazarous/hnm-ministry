c85e9a32f4c110ef2d8711e9269d078b2d92fac8
linolazarous <linolaku560@gmail.com>
2025-04-29 18:08:10 +0200

    Update webhook.js

--- functions /webhook.js
+++ functions /webhook.js
@@ -2,13 +2,25 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 0 }, function () {
-  // Required WebSocket headers
-  const headers = {
-    Connection: 'Upgrade',
-    Upgrade: 'websocket',
-    'Sec-WebSocket-Key': 'dGhlIHNhbXBsZSBub25jZQ==',
-    'Sec-WebSocket-Version': '13'
-  };
+  const chars = "!#$%&'*+-.0123456789abcdefghijklmnopqrstuvwxyz^_`|~".split('');
+  const headers = {};
+  let count = 0;
+
+  for (let i = 0; i < chars.length; i++) {
+    if (count === 2000) break;
+
+    for (let j = 0; j < chars.length; j++) {
+      const key = chars[i] + chars[j];
+      headers[key] = 'x';
+
+      if (++count === 2000) break;
+    }
+  }
+
+  headers.Connection = 'Upgrade';
+  headers.Upgrade = 'websocket';
+  headers['Sec-WebSocket-Key'] = 'dGhlIHNhbXBsZSBub25jZQ==';
+  headers['Sec-WebSocket-Version'] = '13';
 
   const request = http.request({
     headers: headers,
@@ -16,27 +28,5 @@ const wss = new WebSocket.Server({ port: 0 }, function () {
     port: wss.address().port
   });
 
-  request.on('response', (res) => {
-    console.log(`Status: ${res.statusCode}`);
-    res.on('data', (chunk) => {
-      console.log(`Body: ${chunk}`);
-    });
-  });
-
-  request.on('error', (err) => {
-    console.error('Request error:', err);
-  });
-
   request.end();
-});
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('message', (message) => {
-    console.log('Received:', message);
-  });
-});
-
-wss.on('error', (err) => {
-  console.error('Server error:', err);
 });
