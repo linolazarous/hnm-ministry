78e203b861fdaa537fe80767c3261449ef5d5c9f
linolazarous <linolaku560@gmail.com>
2025-04-29 22:01:50 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,15 @@
-import ts from "typescript-eslint";
+import js from "@eslint/js";
+import reactRecommended from "eslint-plugin-react/configs/recommended";
+import hooks from "eslint-plugin-react-hooks";
+
 export default [
-  ...ts.configs.recommended,
-  // rest of your config
+  js.configs.recommended,
+  reactRecommended,
+  {
+    ...hooks.configs.recommended,
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
 ];
