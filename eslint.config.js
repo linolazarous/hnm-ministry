ac742d7d55b6097d34ecacec3d64c1a754656284
linolazarous <linolaku560@gmail.com>
2025-04-29 20:44:12 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,35 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
+import reactRecommended from "eslint-plugin-react/configs/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  reactRecommended,
+  {
+    ...reactHooks.configs.recommended,
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off"
+    }
+  },
+  ...tseslint.configs.recommended
 ]);
