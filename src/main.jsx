ef01bad7ca468bd2c83f82ae3f16615cc053880c
linolazarous <linolaku560@gmail.com>
2025-05-01 08:46:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css' // Optional CSS file
+
+// Initialize Stripe outside React to make it globally available
+window.Stripe = window.Stripe || null;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
