22392f0cd56156cd7c45ed898884ef5176eb245b
linolazarous <linolaku560@gmail.com>
2025-05-05 18:29:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        // Your other externals...
-      ],
-      output: {
-        manualChunks: {
-          aos: ['aos'], // Group AOS separately
-          // Your other chunks...
-        }
-      }
-    }
+    outDir: 'dist'
   }
 })
