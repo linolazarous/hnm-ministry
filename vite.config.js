ac03be8f3b1c0abe46ad590483b88b803219509e
linolazarous <linolaku560@gmail.com>
2025-05-06 20:55:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,23 +12,28 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'public/index.html')
       },
-      
       external: []
     }
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-    
+      '@admin': resolve(__dirname, './src/admin'),
       '@components': resolve(__dirname, './src/components'),
+      '@css': resolve(__dirname, './src/css'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@js': resolve(__dirname, './src/js'),
+      '@lib': resolve(__dirname, './src/lib'),
       '@pages': resolve(__dirname, './src/pages'),
-      '@services': resolve(__dirname, './src/services')
+      '@payment': resolve(__dirname, './src/payment'),
+      '@services': resolve(__dirname, './src/services'),
+      '@utils': resolve(__dirname, './src/utils')
     }
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/css/_variables.scss";`
+        additionalData: `@import "@css/_variables.scss";`
       }
     }
   }
