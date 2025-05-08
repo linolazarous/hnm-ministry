a3a00e053b20639c73443b3415891b5fa8791ffd
linolazarous <linolaku560@gmail.com>
2025-05-08 11:58:55 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('postcss-preset-env')({
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      },
+      autoprefixer: { grid: true }
+    }),
     require('cssnano')({
       preset: 'default'
     })
-    // Add other PostCSS plugins as needed
   ]
-}
+};
