0cef6e32cbdb9d9995dc589086aeb9e7c5b4f489
linolazarous <linolaku560@gmail.com>
2025-05-08 20:11:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig(({ mode }) => {
         '@js': resolve(__dirname, './src/js'),
         '@lib': resolve(__dirname, './src/lib'),
         '@pages': resolve(__dirname, './src/pages'),
-        '@payment': resolve(__dirname, './src/payment'),
+        '@payment': resolve(__dirname, './functions/stripe-payment.js'),
         '@services': resolve(__dirname, './src/services'),
         '@utils': resolve(__dirname, './src/utils')
       }
